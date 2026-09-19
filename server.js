4f5135b104b426a8aa947aa0705a5e529b2c7050
David-E-Alvarez <davidealvarez1@gmail.com>
2026-09-19 16:15:09 -0700

    added fs to read my index.html file (from chatgpt) will use live server now

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello from Node!</h1>");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    //res.end("<h1>Hello from Node!</h1>");
+    fs.readFile("index.html", "utf8", (err, data) => {    
+        if (err) {
+            res.writeHead(500);
+            res.end("Error reading index.html");
+            return;
+        }
+
+        res.end(data);
+    });
 });
 
 server.listen(3000, () => {
